1c428b23ca20c16af4bc6a0eee9fd9776e6fffde
JAVIERA INOSTROZA <jminostroza@uc.cl>
2020-10-26 02:57:23 -0300

    Login fetch is working with g19 backend

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,12 +1,34 @@
 import React, { useState }from "react";
 import { Link } from 'react-router-dom';
+import {url_named_user} from "./../Routes"
 
 import './Join.css';
 import '../general.css'
 
 const Join = () => {
     const [name, setName] = useState('');
+    const [userId, setUserId] = useState('');
     const [room, setRoom] = useState('');
+
+    const handleLoginClick = () => {
+        var requestOptions = {
+            method: 'POST',
+            redirect: 'follow'
+        } 
+        // event.preventDefault();
+        const url_ = url_named_user + name
+        fetch(url_, requestOptions)
+            .then( (response) => {
+                return response.json()
+                .then((data) => {
+                    console.log(data)
+                })
+                
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
     return (
         <div className={'Full'}>
@@ -14,15 +36,14 @@ const Join = () => {
                 <div className={'Inputs'}>
                     <h1> Join </h1>
                     <div><input className='JoinInput' placeholder = "Name" type ="text" onChange={(event) => setName(event.target.value)}/></div>
-                    <div><input className='JoinInput' placeholder = "Room" type ="text" onChange={(event) => setRoom(event.target.value)}/></div>
-                    <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <Link onClick={handleLoginClick} to={`/chat?name=${name}&room=rr`}>
                         <button type="submit"> Sign In</button>
                     </Link>
                 </div>
             </div>
         </div>
 
-    )
+    ) 
 }
 
 export default Join;
