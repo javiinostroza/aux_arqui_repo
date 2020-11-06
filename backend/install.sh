a1f4097aff97a1b08271ee0497002141e80b9ea2
cris <civargas5@uc.cl>
2020-11-06 15:33:15 -0300

    main14

--- backend/install.sh
+++ backend/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-aws--file-exists-behavior RETAIN
 pwd=$( aws ecr get-login-password )
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd https://556070822500.dkr.ecr.us-east-2.amazonaws.com
