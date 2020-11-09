2696d57d924b8336ecfebb6302361d03d68a3c13
luckbox314 <lucas.muoz@uc.cl>
2020-11-09 05:56:52 -0300

    accept request implemented

--- backend/app/controllers/requests_controller.rb
+++ backend/app/controllers/requests_controller.rb
@@ -4,13 +4,15 @@ class RequestsController < ApplicationController
     def accept
         puts "_________________________________-"
         puts "Request accepted"
-        puts params[:url]
-        @request = Request.find_by(url: params[:url])
-        @room = @request.find_by(room_id: @requests.room_id)
+        puts params[:id]
+        @request = Request.find_by(id: params[:id])
+        @room = Room.find_by(id: @request.room_id)
         puts @room
         puts "_________________________________"
-        @room.style_sheet = @requests.url
+        @room.style_sheet = @request.url
         @room.save
+        @request.accepted = true
+        @request.save
         render json: {"success": "true"}
     end
 
