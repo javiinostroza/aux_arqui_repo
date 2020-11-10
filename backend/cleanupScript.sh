2b547f80d1d922610796206ce4311a6122fb5667
cris <civargas5@uc.cl>
2020-11-09 23:44:21 -0300

    try15

--- backend/cleanupScript.sh
+++ backend/cleanupScript.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-rm /home/ec2-user/aux_arqui_repo/backend/*
-rm -rf */
+if [ -d /home/ec2-user/aux_arqui_repo/backend ]; then
+    rm -rf /home/ec2-user/aux_arqui_repo/backend
+fi
+mkdir -vp /home/ec2-user/aux_arqui_repo/backend
