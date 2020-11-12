2a1ec6b4facc9cc3e224eac9236187996ce70bd4
cris <civargas5@uc.cl>
2020-11-12 19:14:40 -0300

    test-fail

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe User, type: :model do
   context 'when user is registered with username' do
     it 'ensures username presence' do
-      user = User.create(username: 'name')
+      user = User.create(username: '')
       expect(user.username).to eq('name')
     end
   end
